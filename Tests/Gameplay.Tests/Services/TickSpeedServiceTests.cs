470f0f34e4be4d2827826a907bcb5eca12978283
Matt <matt@isnor.io>
2025-05-09 21:31:58 -0300

    Cover TickSpeedService with tests, add additional test to mapgen

---
+++ Tests/Gameplay.Tests/Services/TickSpeedServiceTests.cs
@@ -0,0 +1,105 @@
+﻿using Core.Events;
+using Gameplay.Services;
+using Infrastructure.Events;
+
+namespace Gameplay.Tests
+{
+    public class TickSpeedServiceTests
+    {
+        private InMemoryAppEventBus bus;
+        private TickSpeedService service;
+
+        [SetUp]
+        public void Setup()
+        {
+            bus = new InMemoryAppEventBus();
+            service = new TickSpeedService(bus);
+        }
+
+        [Test]
+        public void CurrentMultiplier_DefaultsTo1()
+        {
+            // Assert
+            Assert.That(service.CurrentMultiplier, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SetSpeed_ValidValue_UpdatesCurrentMultiplier()
+        {
+            // Act
+            service.SetSpeed(5);
+
+            // Assert
+            Assert.That(service.CurrentMultiplier, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void SetSpeed_LessThanOne_ThrowsArgumentOutOfRange()
+        {
+            // Act
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => service.SetSpeed(0));
+
+            // Assert
+            StringAssert.Contains("Multiplier must be at least 1", ex.Message);
+        }
+
+        [Test]
+        public void PublishSetSpeedCommand_ChangesMultiplier_And_PublishesSpeedChanged()
+        {
+            // Arrange
+            var received = new List<SpeedChanged>();
+            using (bus.Subscribe<SpeedChanged>(evt => received.Add(evt)))
+            {
+                // Act
+                bus.Publish(new SetSpeedCommand(3));
+
+                // Assert
+                Assert.Multiple(() =>
+                {
+                    Assert.That(service.CurrentMultiplier, Is.EqualTo(3));
+                    Assert.That(received, Has.Count.EqualTo(1));
+                    Assert.That(received[0].newMultiplier, Is.EqualTo(3));
+                });
+            }
+        }
+
+        [Test]
+        public void PublishSetSpeedCommand_SameAsCurrent_DoesNotPublish()
+        {
+            // Arrange
+            var received = new List<SpeedChanged>();
+            using (bus.Subscribe<SpeedChanged>(evt => received.Add(evt)))
+            {
+                // Act
+                bus.Publish(new SetSpeedCommand(1));
+
+                // Assert
+                Assert.Multiple(() =>
+                {
+                    Assert.That(service.CurrentMultiplier, Is.EqualTo(1));
+                    Assert.That(received, Is.Empty);
+                });
+            }
+        }
+
+        [Test]
+        public void PublishSetSpeedCommand_NonPositive_DoesNotPublishOrChange()
+        {
+            // Arrange
+            var received = new List<SpeedChanged>();
+            using (bus.Subscribe<SpeedChanged>(evt => received.Add(evt)))
+            {
+                // Act
+                bus.Publish(new SetSpeedCommand(0));
+                bus.Publish(new SetSpeedCommand(-5));
+
+                // Assert
+                Assert.Multiple(() =>
+                {
+                    Assert.That(service.CurrentMultiplier, Is.EqualTo(1));
+                    Assert.That(received, Is.Empty);
+                });
+            }
+        }
+    }
+}
--- Tests/Gameplay.Tests/Worlds/MapGeneratorTests.cs
+++ Tests/Gameplay.Tests/Worlds/MapGeneratorTests.cs
@@ -2,40 +2,75 @@
 using Gameplay.Worlds;
 using Infrastructure.Config.Interfaces;
 using Infrastructure.Factories;
+using Gameplay.Simulation;
 
-namespace Gameplay.Tests.Worlds;
-
-internal class StubConfigProvider : IConfigProvider
+namespace Gameplay.Tests.Worlds
 {
-    private readonly object _payload;
-    public StubConfigProvider(object payload) => _payload = payload;
-    public T LoadConfig<T>(string fileName) => (T)_payload!;
-}
-
-[TestFixture]
-public class MapGeneratorTests
-{
-    [Test]
-    public void MapGenerator_SingleType_AllEntitiesMatch()
+    internal class StubConfigProvider : IConfigProvider
     {
-        // Arrange
-        var defs = new List<TileDefinition> 
-        {
-            new("only", true, null, 1.0f)
-        };
-        var cfg = new StubConfigProvider(defs);
-        var worldFactory = new DefaultEcsWorldFactory();
-        var rand = new Random(42);
-        var gen = new MapGenerator(cfg, rand);
+        private readonly object _payload;
+        public StubConfigProvider(object payload) => _payload = payload;
+        public T LoadConfig<T>(string fileName) => (T)_payload!;
+    }
 
-        // Act
-        var world = gen.Generate(worldFactory.CreateWorld(), 200, 200);
-
-        // Assert
-        Assert.That(world.GetEntities().AsEnumerable().Count(), Is.EqualTo(200*200));
-        foreach (var e in world.GetEntities().AsEnumerable())
+    [TestFixture]
+    public class MapGeneratorTests
+    {
+        [Test]
+        public void MapGenerator_SingleType_AllEntitiesMatch()
         {
-            Assert.That(e.Get<TileTypeComponent>().Id, Is.EqualTo("only"));
+            // Arrange
+            var defs = new List<TileDefinition>
+            {
+                new("only", true, null, 1.0f)
+            };
+            var cfg = new StubConfigProvider(defs);
+            var worldFactory = new DefaultEcsWorldFactory();
+            var rand = new Random(42);
+            var gen = new MapGenerator(cfg, rand);
+
+            // Act
+            var world = gen.Generate(worldFactory.CreateWorld(), 200, 200);
+
+            // Assert
+            Assert.That(world.GetEntities().AsEnumerable().Count(), Is.EqualTo(200 * 200));
+            foreach (var e in world.GetEntities().AsEnumerable())
+            {
+                Assert.That(e.Get<TileTypeComponent>().Id, Is.EqualTo("only"));
+            }
+        }
+    }
+
+    [TestFixture]
+    public class MapGenerationSystemTests
+    {
+        [Test]
+        public void Update_CalledTwice_OnlyGeneratesOnce()
+        {
+            // Arrange
+            var defs = new List<TileDefinition>
+            {
+                new("only", true, null, 1.0f)
+            };
+            var cfg = new StubConfigProvider(defs);
+            var worldFactory = new DefaultEcsWorldFactory();
+            var rand = new Random(42);
+            var mapGenerator = new MapGenerator(cfg, rand);
+            var system = new MapGenerationSystem(mapGenerator);
+            var world = worldFactory.CreateWorld();
+
+            // Act
+            system.Update(world);
+            var countAfterFirst = world.GetEntities().AsEnumerable().Count();
+            system.Update(world);
+            var countAfterSecond = world.GetEntities().AsEnumerable().Count();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(countAfterFirst, Is.EqualTo(200 * 200));
+                Assert.That(countAfterSecond, Is.EqualTo(countAfterFirst));
+            });
         }
     }
 }
