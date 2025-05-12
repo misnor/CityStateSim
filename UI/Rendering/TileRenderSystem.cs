3821dacf147a0749d30de3749b4e645526121156
Matt <matt@isnor.io>
2025-05-12 18:11:44 -0300

    Visual aspect to trees marked for cuttin

--- UI/Rendering/TileRenderSystem.cs
+++ UI/Rendering/TileRenderSystem.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using CityStateSim.Core.Components;
+using CityStateSim.Core.Components.Tags;
 using DefaultEcs;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using CityStateSim.UI.Factories.Interfaces;
 using CityStateSim.UI.Rendering.Interfaces;
+using CityStateSim.UI;
 
 namespace CityStateSim.UI.Rendering;
 public class TileRenderSystem : IRenderSystem
@@ -37,8 +39,24 @@ public class TileRenderSystem : IRenderSystem
         {
             ref var pos = ref e.Get<PositionComponent>();
             ref var type = ref e.Get<TileTypeComponent>();
-            var rect = new Rectangle(pos.X * tileSize, pos.Y * tileSize, tileSize, tileSize);
+            var rect = new Rectangle(pos.X * Constants.TileSize, pos.Y * Constants.TileSize, Constants.TileSize, Constants.TileSize);
+            
+            // Draw base tile
             spriteBatch.Draw(pixel, rect, TileColors[type.Id]);
+
+            // If it's a tree marked for cutting, draw an overlay
+            if (type.Id == "tree" && e.Has<CutTreeJobTag>())
+            {
+                // Draw a semi-transparent red overlay
+                spriteBatch.Draw(pixel, rect, new Color(255, 0, 0, 100));
+
+                // Draw a red border
+                int borderWidth = 2;
+                spriteBatch.Draw(pixel, new Rectangle(rect.X, rect.Y, rect.Width, borderWidth), Color.Red); // Top
+                spriteBatch.Draw(pixel, new Rectangle(rect.X, rect.Bottom - borderWidth, rect.Width, borderWidth), Color.Red); // Bottom
+                spriteBatch.Draw(pixel, new Rectangle(rect.X, rect.Y, borderWidth, rect.Height), Color.Red); // Left
+                spriteBatch.Draw(pixel, new Rectangle(rect.Right - borderWidth, rect.Y, borderWidth, rect.Height), Color.Red); // Right
+            }
         }
     }
 
