5bae67a459b28a291b96f263c5a188aa3843fbc1
Matt <matt@isnor.io>
2025-05-19 15:23:37 -0300

    remove useless AI comments

--- Gameplay/Simulation/JobAssignmentSystem.cs
+++ Gameplay/Simulation/JobAssignmentSystem.cs
@@ -2,7 +2,6 @@
 using CityStateSim.Core.Components;
 using CityStateSim.Core.Components.Tags;
 using CityStateSim.Core.Enums;
-using CityStateSim.Gameplay.Components;   // for CarryingComponent
 using CityStateSim.Gameplay.Simulation.Interfaces;
 using DefaultEcs;
 using Microsoft.Extensions.Logging;
@@ -20,7 +19,6 @@ namespace CityStateSim.Gameplay.Simulation
 
         public void Update(World world)
         {
-            // gather all idle agents once
             var idleAgents = world.GetEntities()
                 .With<AgentTag>()
                 .With<AgentStateComponent>()
@@ -32,7 +30,6 @@ namespace CityStateSim.Gameplay.Simulation
             if (idleAgents.Count == 0)
                 return;
 
-            // gather all unassigned jobs
             var availableJobs = world.GetEntities()
                 .With<JobComponent>()
                 .With<PositionComponent>()
@@ -48,7 +45,6 @@ namespace CityStateSim.Gameplay.Simulation
                 var job = jobEntity.Get<JobComponent>();
                 var jobPos = jobEntity.Get<PositionComponent>();
 
-                // pick only the agents eligible for this job type
                 var candidates = job.JobType switch
                 {
                     JobType.PickupResource => idleAgents.Where(a => !a.Has<CarryingComponent>()),
@@ -59,7 +55,6 @@ namespace CityStateSim.Gameplay.Simulation
                 if (!candidates.Any())
                     continue;
 
-                // find the nearest eligible agent
                 var nearest = candidates
                     .OrderBy(a =>
                     {
@@ -73,16 +68,13 @@ namespace CityStateSim.Gameplay.Simulation
                 if (!nearest.IsAlive)
                     continue;
 
-                // mark job assigned
                 job.IsAssigned = true;
                 jobEntity.Set(job);
 
-                // send agent walking
                 ref var state = ref nearest.Get<AgentStateComponent>();
                 state.State = AgentState.Walking;
                 nearest.Set(new MovementIntentComponent(jobPos.X, jobPos.Y));
 
-                // remove that agent from this batch so we don't double‐assign
                 idleAgents.Remove(nearest);
             }
         }
